063470d2298896895c1f5b4acd5a04544b2d9df0
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-02-10 23:55:18 +0100

    Create charAt().java

---
+++ charAt().java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Training38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+String word = scanner.nextLine();
+for(int i = 0; i<word.length(); i++){
+    System.out.print(word.charAt(i));
+}
+    }
+}
