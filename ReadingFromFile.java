e057211ae2e80c89be2c201bd825752b8476ebd9
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-02-19 23:50:47 +0100

    Update ReadingFromFile.java

--- ReadingFromFile.java
+++ ReadingFromFile.java
@@ -1,17 +1,12 @@
 package JC;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
-import java.util.Arrays;
 public class ReadingFromFile {
-    public static void main(String[] args) throws FileNotFoundException{
-        String separator = File.separator;
-String path = ("C:"+ separator+ "Users"+ separator+ "holol"+separator+ "Documents"+ separator+ "Folder"+ separator+ "File100");
-File file = new File(path);
-Scanner scanner = new Scanner(file);
-String line = scanner.nextLine();
-String[] words = line.split("");
-        System.out.println(Arrays.toString(words));
-scanner.close();
+    public static void main(String[] args) throws IOException, FileNotFoundException{
+File file = new File(//path of file);
+    Scanner scanner = new Scanner(file);
+    while(scanner.hasNextLine){
+        System.out.println(scanner.nextLine());
+    }
     }
 }
